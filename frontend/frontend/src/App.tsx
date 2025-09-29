b568802de7ad8d26f5d37eb6d4bf619b58da6702
Ramjat19 <b22ee054@iitj.ac.in>
2025-09-29 00:01:26 +0000

    backend and frontend scaffold

---
+++ frontend/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="p-4 text-xl font-bold">
+        Collaborative Code Review Platform 🚀
+      </div>
+    </>
+  )
+}
+
+export default App
